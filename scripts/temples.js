83fab0e1b480ccea3c986372fb42b56de1b45a3f
EmanuelBatixta <emanueljonatas28@gmail.com>
2024-05-02 22:50:54 -0300

    hamButton js

---
+++ scripts/temples.js
@@ -0,0 +1,17 @@
+//Add the current year
+const year = document.querySelector('#currentyear');
+const today = new Date();
+year.innerHTML = `©️ <span id='currentyear'>${today.getFullYear()}</span>`;
+
+//Add the last modification in HTML
+let lastModified = document.lastModified; //define variable
+document.querySelector('#lastModified').innerHTML = `Last Modification: ${lastModified}`; //add in html tag
+
+//The function of hamburguer menu
+const hamButton = document.querySelector('.ham-menu');
+const navigation = document.querySelector('.temples-nav');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
